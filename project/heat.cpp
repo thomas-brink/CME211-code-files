69de7e764f6df6260adfd294de0ea4e73b7dd86e
Thomas Brink <tbrink@stanford.edu>
2021-11-28 01:33:02 -0800

    Add project pt 2 files v2

--- project/heat.cpp
+++ project/heat.cpp
@@ -1,1 +1,81 @@
-// File that we use for the methods of the heat class
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "CGSolver.hpp"
+#include "heat.hpp"
+#include "matvecops.hpp"
+#include "sparse.hpp"
+
+/* Function that takes an input file name for heat equation
+   modeling and sets up its linear system Ax = b. */
+void HeatEquation2D::Setup(std::string inputFile) {
+    std::ifstream inputData(inputFile);
+    int length, width, xNodes, yNodes, sizeA;
+    float h, Tc, Th;
+
+    if (inputData.is_open()) {
+        inputData >> length >> width >> h;
+        inputData >> Tc >> Th;
+        inputData.close()
+    }
+    else {
+        std::cerr << "Error while loading in the input data" << std::endl;
+    }
+
+    xNodes = (int) length/h + 1;
+    yNodes = (int) width/h + 1;
+
+    sizeA = xNodes*yNodes - 2*yNodes - (xNodes - 2);
+    A.resize(sizeA, sizeA);
+
+    // Initialize x (vector of 1s) and b (vector of 0s)
+    for (unsigned int i = 0; i < sizeA; i++) {
+        x.push_back(1.0);
+        b.push_back(0.0);
+    }
+
+    // Set up b (hot isothermal boundary)
+    for (unsigned int k = 0; k < sizeA*sizeA; k+=sizeA) {
+        b[k] = Th;
+    }
+
+    // Set up b (cold isothermal boundary)
+    for (unsigned int l = sizeA-1; l < sizeA*sizeA; l+=sizeA) {
+        b[k] = -Tc*exp((-10*(h*(l%sizeA) - length/2)^2)-2);
+    }
+
+    // Fill up COO-type sparse matrix A
+    for (unsigned int i = 0; i < sizeA; i++) {
+        for (unsigned int j = 0; j < sizeA; j++) {
+            // u_ij coefficient
+            A.addEntry(i, j, 4/h^2);
+            if (i > 0) {
+                // u_i-1,j coefficient
+                A.addEntry(i-1, j, -1/h^2);
+            }
+            if (i < sizeA-1) {
+                // u_i+1,j coefficient
+                A.addEntry(i+1, j, -1/h^2);
+            }
+            // u_i,j-1 and u_i,j+1 coefficients
+            A.addEntry(i, (j-1+sizeA)%sizeA, -1/h^2);
+            A.addEntry(i, (j+1+sizeA)%sizeA, -1/h^2);
+        }
+    }
+
+    A.ConvertToCSR();
+}
+
+/* Function that solves the heat equation linear system using
+   conjugate gradient and writes the progress of this solver
+   to a file with a certain user-defined prefix every 10th
+   iteration. */
+int HeatEquation2D::Solve(std::string soln_prefix) {
+    // Solve heat equations:
+    //  - Call CGSolver function
+    //     - where CGSolver is adjusted to handle SparseMatrix instance -> change input to SparseMatrix, ...
+    //     - and where solution is saved every 10-th iteration
+    //  - Write solutions to files
+}
--- project/sparse.cpp
+++ project/sparse.cpp
@@ -1,1 +1,55 @@
-// File that we use for the methods of the sparse class
+#include <vector>
+
+#include "COO2CSR.hpp"
+#include "matvecops.hpp"
+#include "sparse.hpp"
+
+/* Function that takes in arguments for the number of rows
+   and columns of a sparse matrix and resizes this matrix. */
+void SparseMatrix::Resize(int nrows, int ncols) {
+    // Set number of rows and columns of class instance
+    this->nrows = nrows;
+    this->ncols = ncols;
+
+    // Use only upper left block from current matrix
+    std::vector<int> remove_indices;
+    for (int i = 0; i < i_idx.size(); i--) {
+        if (i_idx[i] > (nrows-1) or j_idx[i] > (ncols-1)) {
+            remove_indices.push_back(i);
+        }
+    }
+
+    for (int r = (remove_indices.size()-1); r >= 0; r--) {
+        i_idx.erase(remove_indices[r]);
+        j_idx.erase(remove_indices[r]);
+        a.erase(remove_indices[r]);
+    }
+}
+
+/* Function that takes in a row index, column index, and
+   corresponding value, and adds this triplet to the
+   COO-type sparse matrix. */
+void SparseMatrix::AddEntry(int i, int j, double val) {
+    if (i >= 0 and i < nrows and j >= 0 and j < ncols) {
+        i_idx.push_back(i);
+        j_idx.push_back(j);
+        a.push_back(val);
+    }
+    else {
+        std::cerr << "Invalid row and/or column index" << std::endl;
+    }
+}
+
+/* Function that converts the COO-type matrix to a CSR-type
+   sparse matrix. This function uses the COO2CSR() function
+   provided. */
+void SparseMatrix::ConvertToCSR() {
+    COO2CSR(a, i_idx, j_idx);
+}
+
+/* Function that takes in a vector and calculates the
+   matrix-vector product of the sparse matrix and this
+   vector. This function is designed for CSR-matrices. */
+std::vector<double> MulVec(std::vector<double> &vec) {
+    return matVecProd(a, i_idx, j_idx, vec);
+}
